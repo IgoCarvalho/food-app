ca98d230767398eee25c7cf35269a475cae5f261
Igo Carvalho <igocarvalho00@gmail.com>
2025-12-04 19:24:00 -0300

    feat: add tailwindcss and shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <Button>Hello World!</Button>
+    </div>
   );
 }
